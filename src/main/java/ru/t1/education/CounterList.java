b6184e6ef53e5f29121d95b643192f2cf639f985
Konstantin Chvilyov <chvilyov.k@iri.center>
2025-12-01 17:42:49 +0300

    Изменено: Все методы явно делегируются в delegate. Класс стал настоящим заместителем (proxy), так как контролирует доступ к реальному объекту через делегирование.
    Это соответствует шаблону Proxy: объект-обёртка, реализующий тот же интерфейс, но добавляющий свою логику (подсчёт операций add).

--- src/main/java/ru/t1/education/CounterList.java
+++ src/main/java/ru/t1/education/CounterList.java
@@ -1,9 +1,8 @@
 package ru.t1.education;
 
-import java.util.AbstractList;
 import java.util.List;
 
-public class CounterList<E> extends AbstractList<E> {
+public class CounterList<E> implements List<E> {
     private final List<E> delegate;
     private static int addCount = 0;
 
@@ -12,13 +11,33 @@ public class CounterList<E> extends AbstractList<E> {
     }
 
     @Override
-    public E get(int index) {
-        return delegate.get(index);
+    public int size() {
+        return delegate.size();
     }
 
     @Override
-    public int size() {
-        return delegate.size();
+    public boolean isEmpty() {
+        return delegate.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return delegate.contains(o);
+    }
+
+    @Override
+    public java.util.Iterator<E> iterator() {
+        return delegate.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return delegate.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return delegate.toArray(a);
     }
 
     @Override
@@ -28,9 +47,86 @@ public class CounterList<E> extends AbstractList<E> {
     }
 
     @Override
+    public boolean remove(Object o) {
+        return delegate.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(java.util.Collection<?> c) {
+        return delegate.containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(java.util.Collection<? extends E> c) {
+        addCount += c.size();
+        return delegate.addAll(c);
+    }
+
+    @Override
+    public boolean addAll(int index, java.util.Collection<? extends E> c) {
+        addCount += c.size();
+        return delegate.addAll(index, c);
+    }
+
+    @Override
+    public boolean removeAll(java.util.Collection<?> c) {
+        return delegate.removeAll(c);
+    }
+
+    @Override
+    public boolean retainAll(java.util.Collection<?> c) {
+        return delegate.retainAll(c);
+    }
+
+    @Override
+    public void clear() {
+        delegate.clear();
+    }
+
+    @Override
+    public E get(int index) {
+        return delegate.get(index);
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return delegate.set(index, element);
+    }
+
+    @Override
     public void add(int index, E element) {
         addCount++;
         delegate.add(index, element);
+    }
+
+    @Override
+    public E remove(int index) {
+        return delegate.remove(index);
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return delegate.indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return delegate.lastIndexOf(o);
+    }
+
+    @Override
+    public java.util.ListIterator<E> listIterator() {
+        return delegate.listIterator();
+    }
+
+    @Override
+    public java.util.ListIterator<E> listIterator(int index) {
+        return delegate.listIterator(index);
+    }
+
+    @Override
+    public java.util.List<E> subList(int fromIndex, int toIndex) {
+        return delegate.subList(fromIndex, toIndex);
     }
 
     public static int getAddCount() {
